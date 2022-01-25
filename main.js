33e1eeea0cfbf9e96c8155147f2f0017ad2b47fc
ojasva2011 <90315077+ojasva2011@users.noreply.github.com>
2022-01-25 15:00:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+// Create a reference for the canvas
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 ))
+		//write a code to check the type of key pressed
+	{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+
+	if((keyPressed >=65 && keyPressed<=90)|| (keyPressed >=97 && keyPressed<=122))
+	{
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet key";
+	}
+
+	if(keyPressed >=48 && keyPressed<=57)
+	{
+		numberkey();
+		document.getElementById("d1").innerHTML="You pressed Number key";
+	}
+
+	if(keyPressed >=37 && keyPressed<=40)
+	{
+		arrowkey();
+		document.getElementById("d1").innerHTML="You pressed Arrow key";
+	}
+
+	if((keyPressed ==17) || (keyPressed==18) || (keyPressed==27))
+	{
+		arrowkey();
+		document.getElementById("d1").innerHTML="You pressed Arrow key";
+	}
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message. 
+document.getElementById("d1").innerHTML="You Have Pressed Alphabet Key"
+console.log("Alpkey.png");
+img_image="Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	document.getElementById("d1").innerHTML="You Have Pressed Number Key"
+console.log("numkey.png");
+img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	document.getElementById("d1").innerHTML="You Have Pressed Arrow Key"
+console.log("Arrkey.png");
+img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	document.getElementById("d1").innerHTML="You Have Pressed Special Key"
+console.log("spkey.png");
+img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	document.getElementById("d1").innerHTML="You Have Pressed Other Key"
+console.log("otherkey.png");
+	img_image="otherkey.png";
+	add();
+}
+	
